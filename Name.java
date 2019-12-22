f5c2c789872794964852402b2068b37be66825b6
Irina Khan <i.r.khan@mail.ru>
2019-12-22 20:32:52 +0300

    Original

---
+++ Name.java
@@ -0,0 +1,11 @@
+public class Name{
+    public static void main(String[] args) {
+        final String Greatings = "Hello, ";
+        final String ending = "!";
+
+        //Вставляем имя
+        String Name = "Irina";
+
+        System.out.println(Greatings + Name+ending);
+    }
+    }
